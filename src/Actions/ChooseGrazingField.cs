d6367123acc088ee45b66a358f9972318810b14d
Brantley Jones <brantleyjones74@gmail.com>
2019-10-21 10:01:11 -0500

    fixed butcher and graze method for sheep

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -6,21 +6,21 @@ using Trestlebridge.Models.Animals;
 
 namespace Trestlebridge.Actions {
     public class ChooseGrazingField {
-        public static void CollectInput (Farm farm, IGrazing animal) {
+        public static void CollectInput(Farm farm, IGrazing animal) {
             Console.Clear();
 
             for (int i = 1; i <= farm.GrazingFields.Count; i++)
             {
-                Console.WriteLine ($"{i}. Grazing Field");
+                Console.WriteLine($"{i}. Grazing Field");
             }
 
-            Console.WriteLine ();
+            Console.WriteLine();
 
             // How can I output the type of animal chosen here?
-            Console.WriteLine ($"Place the animal where?");
+            Console.WriteLine($"Place the animal where?");
 
-            Console.Write ("> ");
-            int choice = Int32.Parse(Console.ReadLine ());
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
 
             farm.GrazingFields[choice - 1].AddResource(animal);
 
