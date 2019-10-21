43ecdb71c2dcfd11c24434e17c32933a9ada3167
Maggie Johnson <mmjohn09@icloud.com>
2019-10-21 13:33:23 -0500

    changed verbage for adding plants

--- src/Models/Plants/Sesame.cs
+++ src/Models/Plants/Sesame.cs
@@ -13,7 +13,7 @@ namespace Trestlebridge.Models.Plants
         }
 
         public override string ToString () {
-            return $"Sesame. Yum!";
+            return $"sesame";
         }
     }
 }
--- src/Models/Plants/Sunflower.cs
+++ src/Models/Plants/Sunflower.cs
@@ -20,7 +20,7 @@ namespace Trestlebridge.Models.Plants
         }
 
         public override string ToString () {
-            return $"Sunflower!";
+            return $"sunflowers";
         }
     }
 }
