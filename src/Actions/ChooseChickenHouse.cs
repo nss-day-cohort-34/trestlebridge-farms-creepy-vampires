5d0be39bfa379937a4c8a845d101472c10de078f
snyder <snyderjosephd@yahoo.com>
2019-10-22 13:32:59 -0500

    Use try/catch on ChooseChickenHouse

--- src/Actions/ChooseChickenHouse.cs
+++ src/Actions/ChooseChickenHouse.cs
@@ -3,29 +3,44 @@ using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
+using System.Threading;
 
 namespace Trestlebridge {
     public class ChooseChickenHouse {
         public static void CollectInput(Farm farm, Chicken chicken) {
             Utils.Clear();
 
-            for (int i = 1; i <= farm.ChickenHouses.Count; i++) {
-                if (farm.ChickenHouses[i - 1].Capacity > farm.ChickenHouses[i - 1].numOfAnimals()) {
-                    Console.WriteLine($"{i}. Chicken House {farm.ChickenHouses[i-1].shortId()} has {farm.ChickenHouses[i - 1].numOfAnimals()} animals.");
-                } else {
-                    Console.WriteLine($"{i}. Chicken House {farm.ChickenHouses[i-1].shortId()} is at capacity with {farm.ChickenHouses[i - 1].numOfAnimals()} animals.");
+            try 
+            {
+                for (int i = 1; i <= farm.ChickenHouses.Count; i++)
+                {
+                    if (farm.ChickenHouses[i - 1].Capacity > farm.ChickenHouses[i - 1].numOfAnimals())
+                    {
+                        Console.WriteLine($"{i}. Chicken House {farm.ChickenHouses[i - 1].shortId()} has {farm.ChickenHouses[i - 1].numOfAnimals()} animals.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i}. Chicken House {farm.ChickenHouses[i - 1].shortId()} is at capacity with {farm.ChickenHouses[i - 1].numOfAnimals()} animals.");
+                    }
                 }
+
+                Console.WriteLine();
+
+                // How can I output the type of animal chosen here?
+                Console.WriteLine($"Place the animal where?");
+
+                Console.Write("> ");
+                int choice = Int32.Parse(Console.ReadLine());
+
+                farm.ChickenHouses[choice - 1].AddResource(chicken);
             }
-
-            Console.WriteLine();
-
-            // How can I output the type of animal chosen here?
-            Console.WriteLine($"Place the animal where?");
-
-            Console.Write("> ");
-            int choice = Int32.Parse(Console.ReadLine());
-
-            farm.ChickenHouses[choice - 1].AddResource(chicken);
+            catch
+            {
+                Console.WriteLine("Please enter a valid selection.");
+                Thread.Sleep(1000);
+                CollectInput(farm, chicken);
+            }
+            
 
             /*
                 Couldn't get this to work. Can you?
