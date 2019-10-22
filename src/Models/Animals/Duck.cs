a4a262f4db8c75711adf83cb9a646b6d983a9041
Brantley Jones <brantleyjones74@gmail.com>
2019-10-22 09:56:43 -0500

    rearranged values grouping all methods together and all properties together for each animal. checked to make sure numbers were correct for each animal

--- src/Models/Animals/Duck.cs
+++ src/Models/Animals/Duck.cs
@@ -8,31 +8,27 @@ namespace Trestlebridge.Models.Animals {
         private Guid _id = Guid.NewGuid();
         private double _feathersProduced = 0.75;
         private int _eggsProduced = 6;
-
+        public double FeedPerDay { get; set; } = 0.8;
+        public string Type { get; } = "Duck";
         private string _shortId {
             get {
                 return this._id.ToString().Substring(this._id.ToString().Length - 6);
             }
         }
 
-        public string Type { get; } = "Duck";
-        public double FeedPerDay { get; set; } = 0.8;
-
         // Methods
         public double Pluck() {
             return _feathersProduced;
         }
-
         public double CollectEggs() {
             return _eggsProduced;
         }
-
+        public void Feed() {
+            Console.WriteLine($"Duck {this._shortId} just ate {this.FeedPerDay}kg of feed.");
+        }
         public override string ToString() {
             return $"Duck {this._shortId}. Quack!";
         }
 
-        public void Feed() {
-            Console.WriteLine($"Duck {this._shortId} just ate {this.FeedPerDay}kg of feed.");
-        }
     }
 }
--- src/Models/Animals/Pig.cs
+++ src/Models/Animals/Pig.cs
@@ -7,26 +7,22 @@ namespace Trestlebridge.Models.Animals {
 
         private Guid _id = Guid.NewGuid();
         private double _meatProduced = 8.4;
-
+        public double GrassPerDay { get; set; } = 3.2;
+        public string Type { get; } = "Pig";
         private string _shortId {
             get {
                 return this._id.ToString().Substring(this._id.ToString().Length - 6);
             }
         }
 
-        public double GrassPerDay { get; set; } = 3.2;
-        public string Type { get; } = "Pig";
-
         // Methods
-        public void Graze () {
+        public void Graze() {
             Console.WriteLine($"Pig {this._shortId} just ate {this.GrassPerDay}kg of grass");
         }
-
-        public double Butcher () {
+        public double Butcher() {
             return _meatProduced;
         }
-
-        public override string ToString () {
+        public override string ToString() {
             return $"Pig {this._shortId}. Oink!";
         }
     }
